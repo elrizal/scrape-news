5f6c8ecae58e1cbee99401ca06145e35ccaaa45b
Erin Rizal <erinrizal@Erins-MacBook-Pro.local>
2018-02-09 16:14:32 -0500

    add

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # scrape-news
+**Mongodb not working
--- app/public/javascript/add-articles.js
+++ app/public/javascript/add-articles.js
@@ -1,4 +1,3 @@
- //On the scrape click, we'll add 20 articles and alert the user with the modal
 
  $(document).ready(function() {
      $('.parallax').parallax();
@@ -13,20 +12,15 @@
               dataType: 'jsonp',
               jsonp: 'jsonp', 
               success: function (data) {
-                console.log('success', data);
+              console.log('success', data);
+    //      var articleTitle = answer[k].children.data;
+    //      var articleLink = answer[k].parent.attribs.href;
+
               },
               error: function (XMLHttpRequest, textStatus, errorThrown) {
                 console.log('error', errorThrown);
               }
             });
-    //      $.ajax({
-    //         url: 'http://localhost:28017/local/andyb',
-    //         method: "GET"
-    //     }).done(function(response) {
-    //      var articlesSpot = $('#articles');
-    //      var answer = response.type;
-    //      var articleTitle = answer[k].children.data;
-    //      var articleLink = answer[k].parent.attribs.href;
-    //     })
+
     });
  });
